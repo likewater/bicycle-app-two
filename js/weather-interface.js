8c647227f3368697d70d1547ab3c10c2604771fc
Blake <lakebayomackway@gmail.com>
2017-04-18 14:32:51 -0700

    add final refactor

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,12 +1,12 @@
 var Weather = require('./../js/weather.js').weatherModule;
 
 var displayHumidity = function(city, humidityData) {
-  $('.showWeather').text("The humidity in " + city + " is " + displayHumidity + "%");
+  $('.showWeather').text("The humidity in " + city + " is " + humidityData + "%");
 }
 
 $(document).ready(function() {
   var currentWeatherObject = new Weather();
-  $('#weatherLocation').click(function() {
+  $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
     currentWeatherObject.getWeather(city, displayHumidity);
--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,11 @@
 var apiKey = require('./../.env').apiKey;
 
-Weather = function(){
+function Weather(){
 }
 
-Weather.prototype.getWeather = function(city, displayHumidity) {
+Weather.prototype.getWeather = function(city) {
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-    displayHumidity(city, response.main.humidity);
+    $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
   }).fail(function(error) {
     $('.showWeather').text(error.responseJSON.message);
   });
