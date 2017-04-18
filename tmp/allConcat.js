1e2c5d784e00f8b222914eb42a3dd85092ebe91c
Blake <lakebayomackway@gmail.com>
2017-04-18 14:02:27 -0700

    add refactor

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -25,3 +25,18 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('#time').text(moment());
 });
+
+var Weather = require('./../js/weather.js').weatherModule;
+
+var displayHumidity = function(city, humidityData) {
+  $('.showWeather').text("The humidity in " + city + " is " + displayHumidity + "%");
+}
+
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    currentWeatherObject.getWeather(city, displayHumidity);
+  });
+});
