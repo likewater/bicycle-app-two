cff8d67e02e58d62979d2b63d21049d69bc5386d
Blake Womack and Kaya Winn <blake+kaya@>
2017-04-19 16:47:19 -0700

    x

--- js/bike.js
+++ js/bike.js
@@ -1,4 +1,6 @@
 var apiKey = require('./../.env').apiKey;
+var test = "true";
+var stolen = "true";
 
 function Bike(location){
   this.location = location;
@@ -23,5 +25,18 @@ Bike.prototype.getLocation = function(location) {
       $('.$showBikes').text(error.responseJSON.message);
       });
     };
+
+
+    Bike.prototype.postStolenBike = function(location, manufacturer, year, frame_model, date_stolen, frameColors, stolen, test) {
+      var stolenBike = {stolen_location :location, manufacturer_name :manufacturer, year :year, frame_model :frame_model, date_stolen :date_stolen, frameColors :frameColors, stolen : "true", test :"true"}
+      $.post('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + manufacturer + '&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
+        locationMake(response.bikes);
+        }));
+      })
+      .fail(function(error) {
+        $('.$showBikes').text(error.responseJSON.message);
+        });
+      };
+
 
 exports.bikeModule = Bike;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,9 +1,5 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
-// var displayResults = function(city, humidityData) {
-//   $('.showBike').text("The humidity in " + city + " is " + humidityData + "%");
-// }
-
 $(document).ready(function() {
   var currentBikeObject = new Bike();
 
@@ -20,6 +16,7 @@ $(document).ready(function() {
         $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
     })
   }
+
   $('#locationMake').click(function() {
     var location = $('#location').val();
     var manufacturer = $('#manufacturer').val();
