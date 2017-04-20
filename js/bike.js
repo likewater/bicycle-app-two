d54c05ff815bdef8594afcf70fad145fce05f6f9
Blake Womack and Kaya Winn <blake+kaya@>
2017-04-20 16:05:01 -0700

    add screwed up code for later

--- js/bike.js
+++ js/bike.js
@@ -3,18 +3,30 @@ var apiKey = require('./../.env').apiKey;
 Bike = function(){
 }
 
+
+//displayLocation(response.bikes);
+
 Bike.prototype.getLocation = function(location) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
-
-    $('.showBikes').text(response.bikes.forEach(function(bike) {
-      $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
-      console.log("response test: " + response);
-    }));
+          displayLocation(response.responseText, true);
+    // $('.showBikes').text(response.bikes.forEach(function(bike) {
+    //   $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
+    //   console.log("response test: " + response);
+    // }));
 
   }).fail(function(error) {
       $('.showBikes').text(error.responseJSON.message);
   });
 };
+
+
+
+
+
+
+
+
+
 
 Bike.prototype.getLocationMake = function(location, manufacturer) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + manufacturer + '&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,5 +1,10 @@
 var Bike = require('./../js/bike.js').bikeModule;
-
+var displayLocation = function(bikes){
+  $('.showBikes').text(response.bikes.forEach(function(bike) {
+    $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
+    console.log("response test: " + response);
+    }));
+};
 
 $(document).ready(function() {
   var currentBikeObject = new Bike();
@@ -9,6 +14,15 @@ $(document).ready(function() {
     $('#location').val();
     currentBikeObject.getLocation(location);
   });
+
+
+
+
+
+
+
+
+
 
   $('#locationMake').click(function() {
     var location = $('#location').val();
