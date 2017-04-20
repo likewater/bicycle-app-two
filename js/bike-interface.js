dd1442f1c99a34f83c72dd7c8682ea6721cfdf53
Kaya Winn <mail@psyreal.net>
2017-04-20 07:39:10 -0700

    adds some base code for google maps

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -30,3 +30,18 @@ $(document).ready(function() {
     });
   });
 });
+
+// -------------------------------------------
+_var mapOptions = {
+    center: new google.maps.LatLng(37.7831,-122.4039),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+new google.maps.Map(document.getElementById('map'), mapOptions);
+
+var markerOptions = {
+    position: new google.maps.LatLng(37.7831, -122.4039)
+};
+var marker = new google.maps.Marker(markerOptions);
+marker.setMap(map);
