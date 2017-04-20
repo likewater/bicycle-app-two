45c50fe68cfb44569ab118207e47360d8405ffc4
Blake Womack and Kaya Winn <kaya@>
2017-04-20 13:47:53 -0700

    add code, revert to unrefactored code, all works.

--- js/bike.js
+++ js/bike.js
@@ -1,17 +1,18 @@
 var apiKey = require('./../.env').apiKey;
 
-function Bike(){
+Bike = function(){
 }
 
 Bike.prototype.getLocation = function(location) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
+
     $('.showBikes').text(response.bikes.forEach(function(bike) {
       $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
-      // console.log(response);
+      console.log("response test: " + response);
     }));
-  })
-    .fail(function(error) {
-      $('.$showBikes').text(error.responseJSON.message);
+
+  }).fail(function(error) {
+      $('.showBikes').text(error.responseJSON.message);
   });
 };
 
@@ -19,12 +20,10 @@ Bike.prototype.getLocationMake = function(location, manufacturer) {
   $.get('https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=' + manufacturer + '&location=' + location + '&distance=25&stolenness=proximity').then(function(response) {
     $('.showBikes').text(response.bikes.forEach(function(bike) {
       $('.showBikes').append("<a href=https://bikeindex.org/bikes/" + bike.id  + "><span><strong>Description</strong>:</span>" + " " + bike.title + "<br>" + "<span><strong>Serial number:</strong></span>" + " " +  bike.serial + "<br>" + "<span><strong>Date reported stolen:</strong></span>" + " " + new Date(bike.date_stolen * 1000) + "</p><br></a>");
-
     }));
   })
     .fail(function(error) {
-      $('.$showBikes').text(error.responseJSON.message);
+      $('.showBikes').text(error.responseJSON.message);
   });
 };
-
 exports.bikeModule = Bike;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,8 +1,5 @@
 var Bike = require('./../js/bike.js').bikeModule;
 
-// var displayResults = function(city, humidityData) {
-//   $('.showBike').text("The humidity in " + city + " is " + humidityData + "%");
-// }
 
 $(document).ready(function() {
   var currentBikeObject = new Bike();
